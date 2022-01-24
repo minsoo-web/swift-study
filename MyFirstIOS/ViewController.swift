25dcf23ee1b2b44addbb7d7786053b4386b307fa
minsoo kim <zlemzlem5656@naver.com>
2022-01-24 14:01:09 +0900

    Initial Commit

---
+++ MyFirstIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstIOS
+//
+//  Created by 빅픽쳐팀 SI on 2022/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
