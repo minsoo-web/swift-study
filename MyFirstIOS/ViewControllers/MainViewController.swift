e9011703a965030b58811c13b690b629afae71da
minsoo kim <zlemzlem5656@naver.com>
2022-01-25 09:56:55 +0900

    subView 추가하기, 애니메이션 추가하기

---
+++ MyFirstIOS/ViewControllers/MainViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  MyFirstIOS
+//
+//  Created by 빅픽쳐팀 SI on 2022/01/24.
+//
+
+import UIKit
+import Lottie
+
+class MainViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .black
+        label.textAlignment = .center
+        label.text = "메인화면"
+        label.font = .boldSystemFont(ofSize: 70)
+        
+        
+        return label
+    }()
+    
+    let animationView: AnimationView = {
+        let animView = AnimationView(name: "lottie")
+        animView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animView.contentMode = .scaleAspectFill
+        return animView
+    }()
+    
+    // 뷰가 생성 되었을 때
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        view.addSubview(animationView)
+        animationView.center = view.center
+        
+        
+        // 애니메이션 실행
+        animationView.play { (finish) in
+            print("애니메이션이 끝났다.")
+            
+            self.view.backgroundColor = .white
+            
+            self.animationView.removeFromSuperview()
+            
+            self.view.addSubview(self.titleLabel)
+            self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            self.titleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+            
+            
+            
+        }
+    }
+}
+
